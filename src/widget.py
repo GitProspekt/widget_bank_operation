f7f95f77cfa08d9e57d801a1c4ce1e542cec7e1c
Dim <kdo_kab.36@mail.ru>
2025-02-08 15:18:19 +0400

    Реализована ф-я Формата даты и Маскировки

--- src/widget.py
+++ src/widget.py
@@ -3,26 +3,28 @@ import Masks
 from mypy.types import TypeStrVisitor
 
 
-# def mask_account_card(num_card_or_acc: str) -> str:
-#     """Ф-я маскировки номера карты или счета"""
-#
-#     if "Счет" in num_card_or_acc:
-#         num_acc = num_card_or_acc[-20:]
-#         num_acc_masks = Masks.get_mask_account(int(num_acc))
-#         masks = num_card_or_acc[:-20] + num_acc_masks
-#     else:
-#         num_card = num_card_or_acc[-16:]
-#         num_card_masks = Masks.get_mask_card_number(int(num_card))
-#         masks = num_card_or_acc[:-16] + num_card_masks
-#     return masks
+def mask_account_card(num_card_or_acc: str) -> str:
+    """Ф-я маскировки номера карты или счета"""
+
+    if "Счет" in num_card_or_acc:
+        num_acc = num_card_or_acc[-20:]
+        num_acc_masks = Masks.get_mask_account(int(num_acc))
+        masks = num_card_or_acc[:-20] + num_acc_masks
+    else:
+        num_card = num_card_or_acc[-16:]
+        num_card_masks = Masks.get_mask_card_number(int(num_card))
+        masks = num_card_or_acc[:-16] + num_card_masks
+    return masks
 
 
 def get_dat(date_unformate: str) -> str:
     """Ф-я преобразование строки в заданный формат даты"""
 
-    new_form_data = date_unformate[0:10:-1]
-    #data_from =
-    return new_form_data
+    day = date_unformate[8:10]
+    month = date_unformate[5:7]
+    year = date_unformate[0:4]
+    return f'{day}.{month}.{year}'
 
 
-print(get_dat("2024-03-11T02:26:18.671407"))
+#print(mask_account_card("MasterCard 7158300734726758"))
+#print(get_dat("2024-03-11T02:26:18.671407"))
