0df95d6a91db6cdc21ee3bedbf873aaea0d45987
Dim <kdo_kab.36@mail.ru>
2025-02-08 14:03:13 +0400

    Добавил ф-ю маскировки карты или счета

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,22 @@
-def mask_account_card():
+import Masks
+
+from mypy.types import TypeStrVisitor
+
+
+def mask_account_card(num_card_or_acc: str) -> str:
+    """Ф-я маскировки номера карты или счета"""
+
+    if "Счет" in num_card_or_acc:
+        num_acc = num_card_or_acc[-20:]
+        num_acc_masks = Masks.get_mask_account(int(num_acc))
+        masks = num_card_or_acc[:-20] + num_acc_masks
+    else:
+        num_card = num_card_or_acc[-16:]
+        num_card_masks = Masks.get_mask_card_number(int(num_card))
+        masks = num_card_or_acc[:-16] + num_card_masks
+    return masks
+
+
+#def get_dat(date_unformate: str) -> str:
+#    """Ф-я преобразование строки в заданный формат даты"""
+
