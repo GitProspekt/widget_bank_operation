4f748f324d0ebbb2c0a10e81bec93829162c0a4d
Dim <kdo_kab.36@mail.ru>
2025-02-11 21:42:10 +0400

    В widget добавил метод .strptime

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 import masks
+from datetime import datetime
 
 
 def mask_account_card(num_card_or_acc: str) -> str:
@@ -6,23 +7,24 @@ def mask_account_card(num_card_or_acc: str) -> str:
 
     if "Счет" in num_card_or_acc:
         num_acc = num_card_or_acc[-20:]
-        num_acc_masks = Masks.get_mask_account(int(num_acc))
-        masks = num_card_or_acc[:-20] + num_acc_masks
+        num_acc_masks = masks.get_mask_account(int(num_acc))
+        masks_fun = num_card_or_acc[:-20] + num_acc_masks
     else:
         num_card = num_card_or_acc[-16:]
-        num_card_masks = Masks.get_mask_card_number(int(num_card))
-        masks = num_card_or_acc[:-16] + num_card_masks
-    return masks
+        num_card_masks = masks.get_mask_card_number(int(num_card))
+        masks_fun = num_card_or_acc[:-16] + num_card_masks
+    return masks_fun
 
 
 def get_dat(date_unformate: str) -> str:
     """Ф-я преобразование строки в заданный формат даты"""
 
-    day = date_unformate[8:10]
-    month = date_unformate[5:7]
-    year = date_unformate[0:4]
-    return f"{day}.{month}.{year}"
+    formatted_date = datetime.strptime(date_unformate, '%Y-%m-%dT%H:%M:%S.%f')
+    return formatted_date.strftime("%d.%m.%Y")
 
 
-# print(mask_account_card("MasterCard 7158300734726758"))
-# print(get_dat("2024-03-11T02:26:18.671407"))
+#print(mask_account_card("MasterCard 7158300734726758"))
+#print(get_dat("2019-07-03T18:35:29.512364"))
+# print(get_dat("2018-06-30T02:08:58.425572"))
+# print(get_dat("2018-09-12T21:27:25.241689"))
+# print(get_dat("2018-10-14T08:21:33.419441"))
