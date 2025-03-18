8eb674f3bfa8c4c41efcbdf7d68f1373bff20d79
Dim <kdo_kab.36@mail.ru>
2025-03-18 11:32:21 +0400

    Исправление ф-и filter_by_currency, написан тест для этой ф-ии в tesy_generators

--- src/generators.py
+++ src/generators.py
@@ -1,37 +1,124 @@
 from typing import Iterator, Dict, List
 
+list_transactionss = [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
 
 
 def filter_by_currency(list_transactions: list[dict], currency: str = "USD") -> Iterator[dict]:
     """
-    Функция должна возвращает итератор, который поочередно выдает транзакции,
+    Функция возвращает итератор, который поочередно выдает транзакции,
     валюта операции поумолчанию: USD)
     """
 
     for transaction in list_transactions:
-        if transaction.get("currency") == currency:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
 
-def transaction_descriptions(transactions: list[dict]) -> iter:
-    """
-    Генератор, который возвращает описание каждой транзакции по очереди.
-    """
-
-    for transaction in transactions:
-        yield transaction["description"]
+# result = filter_by_currency(list_transactionss)
+# print(next(result))
+# print(next(result))
+# print(next(result))
+# print(next(result))
 
 
-def card_number_generator(start: int, end: int) -> iter:
-    """
-    Генератор, который возвращает номера банковских карт
-    в формате XXXX XXXX XXXX XXXX.
-    """
-
-    for number in range(start, end + 1):
-        # Форматируем номер карты: 16 цифр, разделённых пробелами
-        card_number = f"{number:016d}"
-        formatted_card_number = " ".join(
-            [card_number[i:i + 4] for i in range(0, 16, 4)]
-        )
-        yield formatted_card_number
+#
+#
+#
+# def transaction_descriptions(transactions: list[dict]) -> iter:
+#     """
+#     Генератор, который возвращает описание каждой транзакции по очереди.
+#     """
+#
+#     for transaction in transactions:
+#         yield transaction["description"]
+#
+#
+# def card_number_generator(start: int, end: int) -> iter:
+#     """
+#     Генератор, который возвращает номера банковских карт
+#     в формате XXXX XXXX XXXX XXXX.
+#     """
+#
+#     for number in range(start, end + 1):
+#         # Форматируем номер карты: 16 цифр, разделённых пробелами
+#         card_number = f"{number:016d}"
+#         formatted_card_number = " ".join(
+#             [card_number[i:i + 4] for i in range(0, 16, 4)]
+#         )
+#         yield formatted_card_number
