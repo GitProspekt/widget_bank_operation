e29a1a9bd36c02eb26871bffbc8069cdaa53b84f
Dim <kdo_kab.36@mail.ru>
2025-03-19 09:43:35 +0400

    Написан декоратор логирования @log(filename) с записью в файл mylog.txt

---
+++ src/decorators.py
@@ -0,0 +1,53 @@
+import functools
+
+def log(filename=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Формируем строку с входными параметрами
+            inputs = f"Inputs: {args}, {kwargs}"
+
+            try:
+                # Логируем начало выполнения функции
+                log_message = f"{func.__name__} started\n"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(log_message)
+                else:
+                    print(log_message, end='')
+
+                # Выполняем функцию
+                result = func(*args, **kwargs)
+
+                # Логируем успешное завершение функции
+                log_message = f"{func.__name__} ok\n"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(log_message)
+                else:
+                    print(log_message, end='')
+
+                return result
+
+            except Exception as e:
+                # Логируем ошибку, если она возникла
+                log_message = f"{func.__name__} error: {type(e).__name__}. {inputs}\n"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(log_message)
+                else:
+                    print(log_message, end='')
+                raise  # Повторно поднимаем исключение
+
+        return wrapper
+    return decorator
+
+# Пример использования декоратора
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+# Тестируем функцию
+my_function(1, 2)
+#my_function(1, 'a')  # Это вызовет ошибку
+
