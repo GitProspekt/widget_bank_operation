b9b22603bee6f036a3bcea22d24e44d48b539a6f
Dim <kdo_kab.36@mail.ru>
2025-03-22 15:01:40 +0400

    Исправление с info на debug

--- src/Utils.py
+++ src/Utils.py
@@ -4,7 +4,7 @@ import logging
 from src import external_api
 
 logger = logging.getLogger("Utils")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler(
     "C:\\Users\\kdo_k\\PycharmProjects\\widget_bank_operation\\logs\\Utils.log", mode="w", encoding="UTF-8"
 )
@@ -18,12 +18,12 @@ file_way_json = "C:\\Users\\kdo_k\\PycharmProjects\\widget_bank_operation\\data\
 
 def way_json_file(file_way) -> list[dict]:
     """Ф-я рапаковывает json файл в python"""
-    logger.info("Распаковка json файла")
+    logger.debug("Распаковка json файла")
     try:
         with open(file_way, encoding="utf-8") as f:
             data = json.load(f)
         if isinstance(data, list):
-            logger.info("Распаковка json файла завершена")
+            logger.debug("Распаковка json файла завершена")
             return data
         else:
             logger.error("Ошибка! В json файле нет списка")
@@ -51,7 +51,7 @@ data = way_json_file(file_way_json)
 
 def sum_transaction(transaction: dict) -> float:
     """Ф-я выдает сумму транзакции в рублях, конвертирует из USD,EUR в RUB"""
-    logger.info("Конвертация валюты в RUB")
+    logger.debug("Конвертация валюты в RUB")
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
         amount_transaction_RUB = transaction["operationAmount"]["amount"]
     else:
--- src/masks.py
+++ src/masks.py
@@ -3,10 +3,10 @@ import logging
 from mypy.types import Union
 
 logger = logging.getLogger("masks")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler(
-    "C:\\Users\\kdo_k\\PycharmProjects\\widget_bank_operation\\logs\\masks.log", mode="w", encoding="UTF-8"
-)
+    "C:\\Users\\kdo_k\\PycharmProjects\\widget_bank_operation\\logs\\masks.log",
+    mode="w", encoding="UTF-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -16,7 +16,7 @@ def get_mask_card_number(num_card: Union[int]) -> str:
     """Ф-я маскировки номера карты"""
 
     num_card_str = str(num_card)
-    logger.info("Проверка корректности ввода номера карты")
+    logger.debug("Проверка корректности ввода номера карты")
     if len(num_card_str) == 16:
         num_card_two_simbol = num_card_str[4:6]
         num_card_four_one_simbol = num_card_str[0:4]
@@ -32,10 +32,10 @@ def get_mask_account(num_acc: Union[int]) -> str:
     """Ф-я маскировки номера счета"""
 
     num_acc_str = str(num_acc)
-    logger.info("Проверка корректности ввода номера счета")
+    logger.debug("Проверка корректности ввода номера счета")
     if len(num_acc_str) == 20:
         num_acc_four_simbol = num_acc_str[-4:]
-        logger.info("Маскировка счета завершена")
+        logger.debug("Маскировка счета завершена")
         return f"**{num_acc_four_simbol}"
     else:
         logger.error("Ошибка! ValueError: некорректный номер счета")
